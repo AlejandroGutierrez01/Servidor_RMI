35dd0dfdd12de5ec899e8a6f4490903383751b85
AlejandroGutierrez01 <alejandro.gutierrez01@epn.edu.ec>
2025-05-28 23:08:53 -0500

    Fin del Taller

--- src/Servicio/ServidorIImpl.java
+++ src/Servicio/ServidorIImpl.java
@@ -1,45 +1,30 @@
 package Servicio;
 
 import java.rmi.RemoteException;
-import java.rmi.server.RMIClientSocketFactory;
-import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 public class ServidorIImpl extends UnicastRemoteObject implements Servidor {
-    private static ArrayList<Persona> listaPersonas(){
-     ArrayList<Persona> listaPersonas = new ArrayList<Persona>();
-        listaPersonas.add(new Persona(2500,"Admin","alejandro@gmail.com","Alejandro",1));
-        listaPersonas.add(new Persona(2500,"Cargo x","pedro@gmail.com","Pedro",2));
-        listaPersonas.add(new Persona(2500,"Tecnico","marco@gmail.com","Marco",3));
-        listaPersonas.add(new Persona(2500,"Secreterio","polo@gmail.com","Polo",4));
-        listaPersonas.add(new Persona(2500,"Colaborador","cintia@gmail.com","Cintia",5));
-        listaPersonas.add(new Persona(2500,"Admin","carlo@gmail.com","Carlo",6));
-        listaPersonas.add(new Persona(2500,"Admin","david@gmail.com","David",7));
-        listaPersonas.add(new Persona(2500,"Admin","comodo@gmail.com","Comodo",8));
-        listaPersonas.add(new Persona(2500,"Admin","alejandro@gmail.com","Alejandro",9));
-        return listaPersonas;
-    }
-    private static String getPersona(int id){
-        return "Nombre: " + listaPersonas().get(id-1).getNombre()+"\n" +
-                "Correo: " + listaPersonas().get(id-1).getCorreo()+"\n" +
-                "Cargo: " + listaPersonas().get(id-1).getCargo()+"\n"+
-                "Sueldo: " + listaPersonas().get(id-1).getSueldo()+"\n";
-    }
 
     public ServidorIImpl() throws RemoteException {
         super();
     }
+
     @Override
-    public String consultar (int id) throws Exception {
-        if(id<listaPersonas().size()+1){
-            return getPersona(id);
-        }
-        else{
+    public String consultar(int id) throws Exception {
+        ArrayList<Persona> listaPersonas = Consulta.getPersonas();
+
+        // Validar si existe el empleado
+        if (id > 0 && id <= listaPersonas.size()) {
+            Persona persona = listaPersonas.get(id - 1);
+            System.out.println(persona. getNombre() + " " +  persona.getCorreo()+ " " + persona.getCargo()+ " " + persona.getSueldo() );
+            return "Nombre: " + persona.getNombre() + "\n" +
+                    "Correo: " + persona.getCorreo() + "\n" +
+                    "Cargo: " + persona.getCargo() + "\n" +
+                    "Sueldo: " + persona.getSueldo() + "\n";
+
+        } else {
             return "No existen los datos del empleado";
         }
     }
-
 }
-
-
